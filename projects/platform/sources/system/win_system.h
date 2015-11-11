8e897f4ef5b07174715441753d32071560eaa1b4
KabutomoriKenji <kab2168@gmail.com>
2015-11-11 11:03:59 +0900

    インプット用のデファインを追加。

--- projects/platform/sources/system/win_system.h
+++ projects/platform/sources/system/win_system.h
@@ -89,6 +89,8 @@ private:
 
 #define GET_GRAPHIC_DEVICE() WinSystem::GetInstance()->GetGraphicDevice()
 #define GET_DIRECTX9_DEVICE() WinSystem::GetInstance()->GetDirectx9Device()
+#define GET_INPUT_KEYBOARD() WinSystem::GetInstance()->GetKeyboard()
+#define GET_INPUT_MOUSE() WinSystem::GetInstance()->GetMouse()
 
 #endif // _SYSTEM_H_
 
