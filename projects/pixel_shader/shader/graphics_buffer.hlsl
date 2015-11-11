1cb8fa238e1a5c775aaf93a7f2a1a06b59d007c1
KabutomoriKenji <kab2168@gmail.com>
2015-11-11 10:33:26 +0900

    ディファードレンダリングのシェーダファイルを追加。

---
+++ projects/pixel_shader/shader/graphics_buffer.hlsl
@@ -0,0 +1,52 @@
+//*****************************************************************************
+//
+// graphics buffer
+//
+// Author		: Kenji Kabutomori
+//
+//*****************************************************************************
+
+//*****************************************************************************
+// structure declaration
+//*****************************************************************************
+struct IN_PIXEL
+{
+	float4 normal_depth : NORMAL0;
+	float2 texcoord : TEXCOORD0;
+	float4 color : COLOR0;
+	float4 world_position : TEXCOORD1;
+};
+
+struct OUT_PIXEL
+{
+	float4 color : COLOR0;
+	float4 normal_depth : COLOR1;
+	float4 position : COLOR2;
+};
+
+sampler _texture_sampler;
+
+//=============================================================================
+// graphics buffer
+//=============================================================================
+OUT_PIXEL GraphicsBuffer(IN_PIXEL in_pixel,sampler texture_sampler)
+{
+	OUT_PIXEL out_pixel;
+
+	out_pixel.color = 0;
+	out_pixel.normal_depth = 0;
+	out_pixel.position = 0;
+
+	out_pixel.color = in_pixel.color * tex2D(texture_sampler,in_pixel.texcoord);
+	out_pixel.normal_depth = float4(normalize(in_pixel.normal_depth.xyz),in_pixel.normal_depth.w);
+	out_pixel.position = in_pixel.world_position;
+
+	return out_pixel;
+}
+
+OUT_PIXEL main(IN_PIXEL in_pixel)
+{
+	return GraphicsBuffer(in_pixel,_texture_sampler);
+}
+
+//---------------------------------- EOF --------------------------------------
